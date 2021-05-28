95c1969df526d8571e962e5e9763163b6c2c2ec1
dinabandhu50 <beheradinabandhu50@gmail.com>
2021-05-28 16:00:10 +0530

    download data updated

--- src/download_data.py
+++ src/download_data.py
@@ -1,2 +1,29 @@
-# As the kaggle api is not working for me, I am leaving the link using which one can download the data. 
-print("Inside Data Download file")
+import os
+import config
+import zipfile
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+api = KaggleApi()
+api.authenticate()
+
+api.competition_download_files('lish-moa',path=os.path.join(config.ROOT_DIR,'input'))
+
+with zipfile.ZipFile(os.path.join(config.ROOT_DIR,'input','lish-moa.zip'), 'r') as zip_ref:
+    zip_ref.extractall(os.path.join(config.ROOT_DIR,'input','raw'))
+
+# # remove the zip after unziping
+# os.remove(os.path.join(config.ROOT_DIR,'input','lish-moa.zip'))
+
+
+
+
+
+# # Single file download
+# api.competition_download_file(
+#     'lish-moa',
+#     'train_targets_scored.csv',
+#     path=os.path.join(config.ROOT_DIR,'input'))
+
+# # console line to download data
+# kaggle competitions download -c lish-moa
